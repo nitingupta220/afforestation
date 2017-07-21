a2b5c59d7ab2575470896e1016fc0e661a9d5f47
nitingupta220 <nitin16@navgurukul.org>
2017-07-21 15:04:39 +0530

    added video in the index.html and his related changes in the main.css & main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(window).load(function() {
 $(document).ready(function() {
     $.ajax({
      type:"GET",
-       url:"http://api.data.gov.in/resource/89fb1d3e-e7f6-4730-9bd6-ed835f02f738?format=json&api-key=YOURKEY",
+       url:"https://api.data.gov.in/resource/89fb1d3e-e7f6-4730-9bd6-ed835f02f738?format=json&api-key=YOURKEY",
        data: {},
     success: function(data){
          list1 = [];
@@ -76,3 +76,86 @@ $(document).ready(function(){
     });
 
 
+            $(document).ready(function(){
+              // Add scrollspy to <body>
+              $('body').scrollspy({target: ".navbar", offset: 50});   
+
+              // Add smooth scrolling on all links inside the navbar
+              $("#navbar a button").on('click', function(event) {
+                // Make sure this.hash has a value before overriding default behavior
+                if (this.hash !== "") {
+                  // Prevent default anchor click behavior
+                  event.preventDefault();
+
+                  // Store hash
+                  var hash = this.hash;
+
+                  // Using jQuery's animate() method to add smooth page scroll
+                  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                  $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                  }, 800, function(){
+
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                  });
+                }  // End if
+              });
+            });
+            
+
+
+//Video settings 
+var video = document.querySelector('video');
+var container = document.querySelector('#container');
+
+var setVideoDimensions = function () {
+  // Video's intrinsic dimensions
+  var w = video.videoWidth;
+  var h = video.videoHeight;
+  
+  // Intrinsic Ratio
+  // Will be more than 1 if W > H and less if W < H
+  var videoRatio = (w / h).toFixed(2);
+  
+  // Get the container's computed styles
+  //
+  // Also calculate the min dimensions required (this will be
+  // the container dimentions)
+  var containerStyles = window.getComputedStyle(container);
+  var minW = parseInt( containerStyles.getPropertyValue('width') );
+  var minH = parseInt( containerStyles.getPropertyValue('height') );
+  
+  // What's the min:intrinsic dimensions
+  //
+  // The idea is to get which of the container dimension
+  // has a higher value when compared with the equivalents
+  // of the video. Imagine a 1200x700 container and
+  // 1000x500 video. Then in order to find the right balance
+  // and do minimum scaling, we have to find the dimension
+  // with higher ratio.
+  //
+  // Ex: 1200/1000 = 1.2 and 700/500 = 1.4 - So it is best to
+  // scale 500 to 700 and then calculate what should be the
+  // right width. If we scale 1000 to 1200 then the height
+  // will become 600 proportionately.
+  var widthRatio = minW / w;
+  var heightRatio = minH / h;
+  
+  // Whichever ratio is more, the scaling
+  // has to be done over that dimension
+  if (widthRatio > heightRatio) {
+    var newWidth = minW;
+    var newHeight = Math.ceil( newWidth / videoRatio );
+  }
+  else {
+    var newHeight = minH;
+    var newWidth = Math.ceil( newHeight * videoRatio );
+  }
+  
+  video.style.width = newWidth + 'px';
+  video.style.height = newHeight + 'px';
+};
+
+video.addEventListener('loadedmetadata', setVideoDimensions, false);
+window.addEventListener('resize', setVideoDimensions, false);
