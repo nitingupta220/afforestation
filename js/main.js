dc791f93eae8ed114256d75afb511b76caf030ee
nitingupta220 <nitin16@navgurukul.org>
2017-07-22 12:34:05 +0530

    added the typeIt.js plugin

--- js/main.js
+++ js/main.js
@@ -159,3 +159,75 @@ var setVideoDimensions = function () {
 
 video.addEventListener('loadedmetadata', setVideoDimensions, false);
 window.addEventListener('resize', setVideoDimensions, false);
+
+
+
+//            typeIt.js properties start from here
+
+
+
+$("#string1").typeIt({
+    strings: "Afforestation",
+    speed: 80,
+    autoStart: false
+});
+
+$("#string2").typeIt({
+    strings: "Afforestation is the establishment of a forest or stand of trees in an area where there was no previous tree cover. Reforestation is the reestablishment of forest cover, either naturally (by natural seeding, coppice, or root suckers) or artificially (by direct seeding or planting).",
+    speed: 0.1,
+    autoStart: false
+});
+$("#string3").typeIt({
+    strings: "About",
+    speed: 80,
+    autoStart: false
+});
+
+
+$("#string4").typeIt({
+    strings: "Afforestation refers to the process of converting a non-forest land into a forest. Afforestation is highly important to maintain the biodiversity.India is an emerging or developing country whereas the controlling of Carbon emission is very much challenging for the policy makers. The main reason is that India needs much more versatile industries to create jobs for unemployment youths which can directly increase the per capita income and the overall GDP (Gross Domestic Product).Large industries and power plants need a large area to set up its facilities. In India, deforestation has happened in the past for creating Infrastructure (National Highways, Airports, etc.), Industries (Steel, Power, Fast Moving Consumer Goods – FMCG, Engineering, etc.), Mining (Coal, Minerals, Metals, etc.), Oil and Gas exploration, Thermal Power plants, etc.",
+    speed: 0.1,
+    autoStart: false
+});
+$("#string5").typeIt({
+    strings: " Causes of Deforestation",
+    speed: 80,
+    autoStart: false
+});
+$("#string6").typeIt({
+    strings: " We already mentioned that one of the causes of deforestation is harvesting natural resources through logging. Wood is a valuable raw material in construction or manufacturing, and the practice of logging prepares the trees for those purposes.However, forests are often cleared because those in charge of the woodland want to use the area for a different purpose. This is seen in all types of forests and all over the world, but arguably the most devastated forest areas are in the tropics. Tropical forests are forests that span both sides of the Equator, and they are the most diverse ecosystems on the planet.In tropical forests, as well as forests farther away from the Equator, one of the main causes of deforestation is to make room for agriculture. Whether the farming is by individuals who wish to grow only enough food for their own use or performed commercially, agriculture is often cited as the leading cause of deforestation. Cattle ranching is another cause of deforestation. Much like farmers, cattle ranchers clear forests to make room for grazing cattle.",
+    speed: 0.1,
+    autoStart: false
+});
+$("#string7").typeIt({
+    strings: "Why we need Afforestation?",
+    speed: 80,
+    autoStart: false
+});
+$("#string8").typeIt({
+    strings: "Afforestation in drainage basin enhances interception and storage and reduces the surface runoff. This reduces a river’s discharge and so makes it less likely to flood. Afforestation, when combined with floodplain zoning, can be very effective in reducing the risk of flooding. Forests help reduce the impact of flooding by delaying and reducing the size of floods, dispersing the water in a more gradual manner than over bare ground. The tree roots decrease erosion and stabilize the soil, this means that less soil is carried by surface runoff and through flow into the river, reducing the channel capacity. Due to afforestation new habitats are created for wildlife. Vast destruction of forest areas leaves animals homeless. Forests provide a home, food source and place of protection for most species of animal. Therefore planting new trees and plant and creating forest lands will provide shelter to these animals. It will also provide grazing facilities and solve the problem of fodder for cattle.",
+    speed: 0.1,
+    autoStart: false
+});
+$("#string9").typeIt({
+    strings: "Steps taken by Govt.",
+    speed: 80,
+    autoStart: false
+});
+$("#string10").typeIt({
+    strings: "As per India State of Forest Report-2013, the total forest cover in the country is 697,898 square kilometers which is 21.23% of the total geographical area of the country. There is a net increase of 5871 square kilometers in the forest cover of the country as compared with India State of Forest Report-2011. It is also a fact that the density of population has increased in the State of Maharashtra, Karnataka, Chhattisgarh, Odisha and Jharkhand. In order to increase the forest and tree cover and improve the quality of existing forest, afforestation in the country is taken up under various Centrally Sponsored Schemes such as National Afforestation Programme (NAP), Mahatma Gandhi National Rural Employment Guarantee Scheme (MGNREGS), Integrated Watershed Management Programme (IWMP), National Bamboo Mission (NBM) and under Compensatory Afforestation Fund Management and Planning Authority (CAMPA), 13th Finance Commission Grant, different State Plan/ Non- Plan Schemes including externally aided projects. National Afforestation Programme (NAP) of the Ministry of Environment & Forests is a 100% Centrally Sponsored Scheme for Afforestation and tree plantation and eco-restoration of degraded forests and adjoining areas in the country. The Scheme is being implemented through a decentralized mechanism of State Forest Development Agency (SFDA) at State level, Forest Development Agency (FDA) at Forest Division level and Joint Forest Management Committees (JFMCs) at Village levels. The Ministry has also released funds to the tune of Rs.4994.55 lakhs and 1265.50 lakhs to the states in the financial year 2012-13 and 2013-14 for preparatory activities for implementation of Green India Mission. The activities undertaken by the State Governments under preparatory phase include institutional strengthening, training, identification of landscapes and preparation of Perspective Plan for implementation of Green India Mission.",
+    speed: 0.1,
+    autoStart: false
+});
+$("#string11").typeIt({
+    strings: "Conclusion",
+    speed: 80,
+    autoStart: false
+});
+$("#string12").typeIt({
+    strings: "Afforesting is a positive effort in curbing the over-use and destruction of natural forests. If done with proper planning and at appropriate sites, it can become a commercially viable solution for many human needs without harming the balance of nature.",
+    speed: 0.1,
+    autoStart: false
+});
+
+
